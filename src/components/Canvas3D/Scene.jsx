d3ddf29d604497d0612e153ee9494f2a8646e670
jessejp <jessepernila@gmail.com>
2023-07-09 21:00:54 +0300

    Updated hero and contact section

--- src/components/Canvas3D/Scene.jsx
+++ src/components/Canvas3D/Scene.jsx
@@ -11,8 +11,8 @@ const Scene = () => {
   useEffect(() => {
     console.log(state.size.width, responsiveCenter);
     if (responsiveCenter) {
-      state.camera.position.set(0.1, 0.5, 1.5);
-      state.camera.lookAt(0.6, 0.25, 0);
+      state.camera.position.set(0.4, 0.45, 1.75);
+      state.camera.lookAt(1.2, 0.25, 0);
     } else {
       state.camera.position.set(0, 0.8, 2);
       state.camera.lookAt(0, 0, 0);
@@ -21,6 +21,13 @@ const Scene = () => {
 
   return (
     <>
+      <MovingScenery
+        position={[4, -0.1, 0]}
+        rotation={[0, Math.PI * 0.2, 0]}
+        meshRotation={[Math.PI * 2, 1.2, 2.3]}
+        shape={<coneGeometry args={[2, 2, 8, 1]} />}
+        spawnOffset={9}
+      />
       <MovingScenery
         position={[1.4, 0, 0]}
         rotation={[0, Math.PI * 0.2, 0]}
@@ -31,14 +38,21 @@ const Scene = () => {
         position={[1.4, 0, 0]}
         rotation={[0, Math.PI * 0.2, 0]}
         shape={<boxGeometry args={[1, 2, 1]} />}
-        spawnOffset={12}
+        spawnOffset={7}
       />
       <MovingScenery
         position={[-1.4, -0.1, 0]}
         rotation={[0, Math.PI * 0.2, 0]}
         meshRotation={[0, 1, 0]}
         shape={<coneGeometry args={[0.8, 1.5, 8, 1]} />}
-        spawnOffset={9}
+        spawnOffset={6}
+      />
+      <MovingScenery
+        position={[0, -0.5, 0]}
+        rotation={[0, Math.PI * 0.2, 0]}
+        meshRotation={[Math.PI / 2, 0, 0]}
+        shape={<planeGeometry args={[2, 2]} />}
+        spawnOffset={3}
       />
       <MovingScenery
         position={[0, -0.5, 0]}
@@ -52,14 +66,14 @@ const Scene = () => {
         rotation={[0, Math.PI * 0.2, 0]}
         meshRotation={[Math.PI / 2, 0, 0]}
         shape={<planeGeometry args={[2, 2]} />}
-        spawnOffset={9}
+        spawnOffset={7}
       />
       <MovingScenery
         position={[0, -0.5, 0]}
         rotation={[0, Math.PI * 0.2, 0]}
         meshRotation={[Math.PI / 2, 0, 0]}
         shape={<planeGeometry args={[2, 2]} />}
-        spawnOffset={12}
+        spawnOffset={9}
       />
       <RunningMan rotation={[0, Math.PI * 0.2, 0]} position={[0, -1, -0.5]} />
     </>
