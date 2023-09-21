37ed8d04e2b74576c59e29502b653fb11a6acf95
jessejp <jessepernila@gmail.com>
2023-09-21 07:26:31 +0300

    added vercel analytics to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,11 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-  ]
+  ],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
